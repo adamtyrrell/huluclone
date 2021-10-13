e9dfc65a2ba61ee8b4840f3e952e58ad64b7a9f4
Adam Tyrrell <tyrrell_adam@yahoo.com>
2021-10-13 12:06:36 -0500

    padding change for results container

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ import { forwardRef } from "react";
 const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div ref={ref} className="p-2 group cursor-pointer transition duration-200
+        <div ref={ref} className="p-1 sm:p-2 group cursor-pointer transition duration-200
         ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
                 layout='responsive'
