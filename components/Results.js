28699610e18cef8fa3ff555a72a7572561398882
Adam Tyrrell <tyrrell_adam@yahoo.com>
2021-10-13 11:52:35 -0500

    margin for results updated for mobile

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from "react-flip-move";
 
 function Results({ results }) {
     return (
-        <div className="px-1 m-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+        <div className="px-0 m-1 sm:m-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result => (
                 <Thumbnail key={result.id} result={result}/>
             ))}
